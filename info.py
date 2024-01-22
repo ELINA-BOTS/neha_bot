60c3b975354b1d766b34a13c75cb8c126c0a0348
Jij1111 <157368327+Jij1111@users.noreply.github.com>
2024-01-22 18:17:19 +0530

    Update info.py

--- info.py
+++ info.py
@@ -48,7 +48,7 @@ SUPPORT_CHAT_ID = int(support_chat_id) if support_chat_id and id_pattern.search(
 NO_RESULTS_MSG = bool(environ.get("NO_RESULTS_MSG", False))
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://zishan:zishan@cluster0.cbvu2lp.mongodb.net/?retryWrites=true&w=majority")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://Zishan:Zishan@cluster0.jiljdqf.mongodb.net/?retryWrites=true&w=majority")
 DATABASE_NAME = environ.get('DATABASE_NAME', "cluster0")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
